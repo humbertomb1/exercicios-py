4357d64ac9146138b00f8b6bc423d9dace7b5a1e
hmbriito <hmbriito@gmail.com>
2023-10-02 23:49:34 -0300

    exercicios criados

--- ex077.py
+++ ex077.py
@@ -5,7 +5,6 @@ com uma contagem exetenso, de zero até 20
 Seu programa deverá ler um número pelo teclado (entre 0 e 20)
 e mostrá-lo po extenso
 input : Digite um número entre 0 e 20:
-<<<<<<< HEAD
 '''
 
 tupla = ('Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito','Nove', 'Dez')
@@ -13,6 +12,3 @@ tupla = ('Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito','Nove'
 num = int(input('Digite um número: '))
 
 print(tupla[num -1])
-=======
-'''
->>>>>>> 1697bb79e933a69e4f690dca8c9af252bb0a42be
--- ex079.py
+++ ex079.py
@@ -5,7 +5,6 @@ colocar em uma tupla
 Depois disso, mostre a listagem de números gerados
 e também o menor e o maior valor que estão na tupla
 # Talvez criar cinco tuplas e depois contatenálos em uma tupla?
-<<<<<<< HEAD
 '''
 import random
 
@@ -17,6 +16,3 @@ for c in tupla:
 
 print(f'\n\nMenor valor: {min(tupla)}')
 print(f'\nMaior valor: {max(tupla)}')
-=======
-'''
->>>>>>> 1697bb79e933a69e4f690dca8c9af252bb0a42be
