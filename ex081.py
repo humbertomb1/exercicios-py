e1ec71d10e37248cc095c1e5de13040c79d77744
hmbriito <hmbriito@gmail.com>
2023-10-02 23:46:31 -0300

    teste

--- ex081.py
+++ ex081.py
@@ -7,7 +7,7 @@ No final, mostre uma listagem de preços, organizados os dados em forma tabular
 '''
 
 tupla = iter(('Lapis', 2.50, 'Borracha', 3.00, 'Caderno', 15.00, 'Mochila', 50.00,))
-
+ 
 for items in tupla:
     preço = next(tupla)
     print(f'{items}.............: {preço:.2f}')
